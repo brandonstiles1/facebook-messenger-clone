e29da7789ca27c77dccc88066938c16a8fe3af8a
brandonstiles1 <brandonstiles@gmail.com>
2023-10-01 23:03:00 -0400

    Adjusts how providers are exported

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -17,30 +17,33 @@ import { AuthGuard } from './auth.guard';
 })
 export class SharedModule {
   static registerRmq(service: string, queue: string): DynamicModule {
+    const providers = [
+      {
+        provide: service,
+        useFactory: (configService: ConfigService) => {
+          const USER = configService.get('RABBITMQ_USER');
+          const PASSWORD = configService.get('RABBITMQ_PASS');
+          const HOST = configService.get('RABBITMQ_HOST');
+
+          return ClientProxyFactory.create({
+            transport: Transport.RMQ,
+            options: {
+              urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+              queue,
+              queueOptions: {
+                durable: true,
+              },
+            },
+          });
+        },
+        inject: [ConfigService],
+      },
+    ];
+
     return {
       module: SharedModule,
-      providers: [
-        {
-          provide: service,
-          useFactory: (configService: ConfigService) => {
-            const USER = configService.get('RABBITMQ_USER');
-            const PASSWORD = configService.get('RABBITMQ_PASS');
-            const HOST = configService.get('RABBITMQ_HOST');
-
-            return ClientProxyFactory.create({
-              transport: Transport.RMQ,
-              options: {
-                urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-                queue,
-                queueOptions: {
-                  durable: true,
-                },
-              },
-            });
-          },
-          inject: [ConfigService],
-        },
-      ],
+      providers,
+      exports: providers,
     };
   }
 }
